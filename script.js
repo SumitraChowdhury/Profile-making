388ba55b2c709104b28813bac524a9ca4697f7bd
SumitraChowdhury <sumitrachow071@gmail.com>
2026-07-16 19:53:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let background= document.querySelector(".background")
+let picture= document.querySelectorAll("img")
+
+picture.addEventListener("click", function(){
+    img.classList.add("imgOne")
+})
