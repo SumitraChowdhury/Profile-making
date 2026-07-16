0bc98fa8a11e17f229ed451361badd4b5df098a1
SumitraChowdhury <sumitrachow071@gmail.com>
2026-07-16 20:05:37 +0600

    project

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let heading=document.querySelector(".heading")
 let background= document.querySelector(".background")
 let picture= document.querySelectorAll("img")
+let button=document.querySelector(".button")
 
 picture.addEventListener("click", function(){
     img.classList.add("imgOne")
+})
+
+
+
+button.addEventListener("click", function(){
+    heading.innerHTML="Contact number=01522123487"
 })
